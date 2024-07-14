afdc39d885d342475cb29135f38a8b1373064727
caioocardoso <136840071+caioocardoso@users.noreply.github.com>
2024-07-14 14:46:22 -0300

    Atualizar o 2024.1

---
+++ 2024.1/q01.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+Escreva um programa em C, que dada a distância total, em quilômetros, percorrida por uma certa
+partícula trafegando do emissor até algum sensor, calcule e informe qual será o sensor atingido
+pela partícula. Por exemplo, veja que se a distância total for 23 quilômetros, então a partícula tem
+que ter atingido o sensor 2. 
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    int dist;
+    scanf("%d", &dist);
+
+    dist= ((dist - 3)%8)-2;
+    printf("Sensor: %d", dist);
+}
