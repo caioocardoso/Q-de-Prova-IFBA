839972389f4b9d3d0b65dcc1822d0737ec0b933b
caioocardoso <136840071+caioocardoso@users.noreply.github.com>
2024-07-17 18:05:42 -0300

    Atualizar o q01.c

--- 2016.2/q01.c
+++ 2016.2/q01.c
@@ -1,1 +1,16 @@
-a
+#include <stdio.h>
+
+int main(){
+    int i, j, m;
+    scanf("%d %d %d", &i,&j,&m);
+    
+    if(i%m==j%m){
+        for(int iCont=1; iCont<=i; iCont++){
+            for(int kCont=1; kCont<=j; kCont++){
+                if(iCont%m==kCont%m){
+                    printf("%d e %d\n", iCont, kCont);
+                }
+            }
+        }
+    }
+}
